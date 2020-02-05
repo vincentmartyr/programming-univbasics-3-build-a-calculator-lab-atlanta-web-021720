00f47ed1d8ce18a2fac14e0436ac5c2859b13819
Vincent Martyr <vincentmartyr@gmail.com>
2020-02-05 19:05:30 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,23 @@
 def addition(num1, num2)
-
+p num1 + num2
 end
 
 def subtraction(num1, num2)
-
+p num1 - num2
 end
 
 def division(num1, num2)
-
+p num1/num2
 end
 
 def multiplication(num1, num2)
-
+p num1 * num2
 end
 
 def modulo(num1, num2)
-
+p num1.0/num2.0
 end
 
 def square_root(num)
-
+p num **
 end
